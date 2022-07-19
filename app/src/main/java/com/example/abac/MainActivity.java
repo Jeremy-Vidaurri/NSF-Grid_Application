b3aba7a1dba3764c3d36e23ff5713228400c4bee
Jeremy-Vidaurri <vidaurri77.jv@gmail.com>
2022-07-19 10:44:16 -0500

    Add comments and fix TODO list.

--- app/src/main/java/com/example/abac/MainActivity.java
+++ app/src/main/java/com/example/abac/MainActivity.java
@@ -1,23 +1,18 @@
 package com.example.abac;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
 import android.widget.SimpleCursorAdapter;
 import android.widget.Spinner;
 
-import java.util.List;
+import androidx.appcompat.app.AppCompatActivity;
 
 
 /* TODO:
   * Implement method to send the matrix to drone.
-  * Implement a method to get current policyID.
   * Implement a screen to add a new policy.
   * Implement error checking for fields before adding a new policy.
-  * Display the currently loaded policy in spinner.
   * Add a button delete the currently loaded policy. Don't allow the user to delete the last policy.
   * Implement yellow zones
 */
@@ -26,7 +21,7 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     private customView grid;
-    private dbHelper dbHelper = new dbHelper(MainActivity.this);
+    private final dbHelper dbHelper = new dbHelper(MainActivity.this);
     private SQLiteDatabase db;
     private PolicyModel policyModel;
     private int curPolicy;
@@ -40,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
         updateSpinner();
 
         // Check if the table is empty. If so, create a 20x20 table
-        Cursor cur = db.rawQuery("SELECT COUNT(*) FROM " + com.example.abac.dbHelper.POLICY_TABLE_NAME, null);
+        Cursor cur = db.rawQuery("SELECT COUNT(*) FROM Policies", null);
 
         if (cur!=null){
             cur.moveToFirst();
@@ -48,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
                 policyModel = new PolicyModel(-1, "Example Policy",20);
                 dbHelper.addPolicy(policyModel);
                 dbHelper.initMatrix(1,20);
+                curPolicy=1;
                 grid.initGrid(20);
             }else{
                 loadFirstPolicy();
@@ -60,13 +56,15 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-
+    // Load the first policy. Used on initial load and when deleting the current policy.
     public void loadFirstPolicy(){
+        // Select the first policyID in the table
         Cursor cur = db.rawQuery("SELECT MIN(PolicyID) FROM Policies",null);
         cur.moveToFirst();
         curPolicy = cur.getInt(0);
         cur.close();
 
+        // Select the size from the specified policyID so that we may initialize the grid.
         cur = db.rawQuery("SELECT size FROM Policies WHERE PolicyID=" + curPolicy,null);
         cur.moveToFirst();
         int size = cur.getInt(0);
@@ -75,7 +73,10 @@ public class MainActivity extends AppCompatActivity {
         cur.close();
     }
 
+    // Load all policy names into the spinner and associate the id with it.
     public void updateSpinner(){
+        // Select the policyID under _id so that we may use it in the spinner
+        // Select Policy names so that we may display it in the spinner.
         Cursor cur = db.rawQuery("SELECT PolicyID as _id, PolicyName FROM Policies",null);
         String[] orig = new String[]{"PolicyName"};
         int[] dest = new int[]{android.R.id.text1};
